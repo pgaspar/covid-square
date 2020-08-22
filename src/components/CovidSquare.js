38daf1e2f69e30bc16287b2a1e097fb713cc8a5d
Pedro Gaspar <pedro.gaxpar@gmail.com>
2020-08-22 09:50:48 +0100

    Improve CovidSquare dimensions code

--- src/components/CovidSquare.js
+++ src/components/CovidSquare.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import './CovidSquare.css';
 
-function CovidSquare({ width = 500, total, cases, deaths }) {
-  const totalPixels = width * width;
-  const casesPixels = (cases * totalPixels) / total;
-  const deathsPixels = (deaths * totalPixels) / total;
-
-  const totalStyle = {
-    width: `${width}px`,
-    height: `${width}px`,
-  };
+function CovidSquare({ total, cases, deaths }) {
+  const casesPercentage = cases / total;
+  const deathsPercentage = deaths / total;
 
   const casesStyle = {
-    width: `${Math.sqrt(casesPixels / totalPixels) * 100}%`,
-    height: `${Math.sqrt(casesPixels / totalPixels) * 100}%`,
+    width: `${Math.sqrt(casesPercentage) * 100}%`,
+    height: `${Math.sqrt(casesPercentage) * 100}%`,
   };
 
   const deathsStyle = {
-    width: `${Math.sqrt(deathsPixels / casesPixels) * 100}%`,
-    height: `${Math.sqrt(deathsPixels / casesPixels) * 100}%`,
+    width: `${Math.sqrt(deathsPercentage / casesPercentage) * 100}%`,
+    height: `${Math.sqrt(deathsPercentage / casesPercentage) * 100}%`,
   };
 
   return (
-    <div className='CovidSquare' style={totalStyle}>
+    <div className='CovidSquare'>
       <div className='CovidSquare-cases' style={casesStyle}>
         <div className='CovidSquare-deaths' style={deathsStyle}></div>
       </div>
