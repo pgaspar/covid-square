0f1a5af493bca250e694581bb69e5be389401f71
Pedro Gaspar <pedro.gaxpar@gmail.com>
2020-08-22 18:38:54 +0100

    Use country slugs & always fallback to Portugal

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import CovidSquare from './components/CovidSquare.js';
 import Head from './components/Head.js';
 import axios from 'axios';
 import Select from 'react-select';
-import { countryOptions } from './Countries.js';
+import { countryOptions, findCountry } from './Countries.js';
+
+const FALLBACK_COUNTRY = 'portugal';
 
 function App() {
   const urlParams = new URLSearchParams(window.location.search);
-  const defaultCountry = urlParams.get('country') || 'Portugal';
+  let initialCountry = findCountry(urlParams.get('country'));
+  if (!initialCountry) {
+    initialCountry = findCountry(FALLBACK_COUNTRY);
+    window.history.replaceState({}, null, `?country=${initialCountry.slug}`);
+  }
 
-  const [country, setCountry] = useState(defaultCountry);
+  const [country, setCountry] = useState(initialCountry);
   const [data, setData] = useState({});
   const { cases, deaths, population } = data;
 
   useEffect(() => {
     async function fetchData() {
       const result = await axios.get(
-        `https://disease.sh/v3/covid-19/countries/${country}`
+        `https://disease.sh/v3/covid-19/countries/${country.name}`
       );
 
       setData(result.data);
@@ -26,8 +32,9 @@ function App() {
   }, [country]);
 
   const onSelect = (e) => {
-    window.history.pushState({}, null, `?country=${e.value}`);
-    setCountry(e.value);
+    const newCountry = findCountry(e.value) || findCountry(FALLBACK_COUNTRY);
+    window.history.pushState({}, null, `?country=${newCountry.slug}`);
+    setCountry(newCountry);
   };
 
   return (
@@ -40,7 +47,7 @@ function App() {
           placeholder='Select a country...'
           className='App-select'
         />
-        <h1>{`Covid cases and deaths: ${country}`}</h1>
+        <h1>{`Covid cases and deaths: ${country.name}`}</h1>
         <CovidSquare total={population} cases={cases} deaths={deaths} />
       </header>
     </div>
--- src/components/Head.js
+++ src/components/Head.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 function Head({ country }) {
-  const title = `Covid Square: ${country}`;
+  const title = `Covid Square: ${country.name}`;
   const author = 'Pedro Gaspar';
   const description = `Cases and deaths compared to total population`;
   const publicUrl = process.env.URL || 'https://covid-square.netlify.app';
