3266cc021dd5e8e48e688dfc2282da9c7b4459c4
Pedro Gaspar <pedro.gaxpar@gmail.com>
2020-11-17 11:36:36 +0000

    Simplify Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,17 +3,11 @@ import { Helmet } from 'react-helmet';
 
 function Head({ country }) {
   const title = `Covid Square: ${country.name}`;
-  const author = 'Pedro Gaspar';
-  const description = `Cases and deaths compared to total population`;
   const publicUrl = process.env.URL || 'https://covid-square.com';
 
   return (
     <Helmet>
       <title>{title}</title>
-
-      <meta name='author' content={author} />
-      <meta name='description' content={description} />
-
       <link rel='canonical' href={publicUrl} />
     </Helmet>
   );
