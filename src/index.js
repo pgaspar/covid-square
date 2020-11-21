03d1d6f0e079a8fe42ff3d472860456d9138aa7d
Pedro Gaspar <pedro.gaxpar@gmail.com>
2020-11-21 00:59:42 +0000

    Remove Sentry's tracing feature

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn:
       'https://237992fcb9824a9b88832db09050b309@o454706.ingest.sentry.io/5445158',
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
   });
 }
 
