7fbac8a640b66553f63003ffa802381f738f6eaf
Pedro Gaspar <pedro.gaxpar@gmail.com>
2020-12-08 20:07:13 +0000

    Extract Button CSS class

--- src/components/BuyMeACoffee.js
+++ src/components/BuyMeACoffee.js
@@ -5,13 +5,15 @@ import './BuyMeACoffee.css';
 function BuyMeACoffee() {
   const url = 'https://www.buymeacoffee.com/pedrogaspar';
   return (
-    <a href={url} target='_blank' rel='noreferrer' className='BuyMeACoffee'>
-      <img
-        src={whiteLogo}
-        alt='Buy Me A Coffee'
-        height='35px'
-        width='126.6px'
-      />
+    <a href={url} target='_blank' rel='noreferrer'>
+      <div className='Button BuyMeACoffee'>
+        <img
+          src={whiteLogo}
+          alt='Buy Me A Coffee'
+          height='35px'
+          width='126.6px'
+        />
+      </div>
     </a>
   );
 }
